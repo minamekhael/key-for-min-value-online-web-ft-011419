20db63ff85220fc65477efa4ba97f8feee5d6772
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-18 21:37:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,9 @@ def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil
   name_hash.each do |key, value|
-  if 
-    lowest_value == 0 || value < lowest_value
-    lowest_value == value
-      lowest_value
-  elsif
-    lowest_key == key
-      key
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value 
+      lowest_key = key
     end
   end
   lowest_key
